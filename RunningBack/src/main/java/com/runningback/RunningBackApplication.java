2eebbb602ebf3a254210313b3a5416d47bbff94d
Roberto Lopez Barbarin <rlopezbarbarin@gmail.com>
2024-07-24 21:18:15 +0200

    Primer commit

---
+++ RunningBack/src/main/java/com/runningback/RunningBackApplication.java
@@ -0,0 +1,13 @@
+package com.runningback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningBackApplication.class, args);
+	}
+
+}
---
+++ RunningBack/src/test/java/com/runningback/RunningBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runningback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
