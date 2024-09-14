7523a5c0c6c4fcec8d58a0a29c816ffc99b65700
Roberto Lopez Barbarin <rlopezbarbarin@gmail.com>
2024-09-14 15:26:58 +0200

    Cambios pequños y de español a inglés registro

--- RunningFront/src/app/auth/register/register.component.ts
+++ RunningFront/src/app/auth/register/register.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { Country } from '../../interfaces/country';
 import { GetsService } from '../../services/gets.service';
+import { User } from '../../interfaces/user';
 
 @Component({
   selector: 'app-register',
@@ -16,50 +17,66 @@ export class RegisterComponent {
 
   countries: Country[];
   registerForm: FormGroup;
+  submitted = false;
 
 
-  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router, private countryService: GetsService) { 
-    this.registerForm = formBuilder.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router, private countryService: GetsService) { }
+
+  ngOnInit() {
+
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required, Validators.minLength(3)],
+      lastName: ['', Validators.required, Validators.minLength(3)],
       email: ['', [Validators.required, Validators.email]],
-      username: ['', Validators.required],
+      username: ['', Validators.required, Validators.minLength(3)],
       password: ['', [Validators.required, Validators.minLength(8)]],
       password2: ['', Validators.required],
-      weight: ['', Validators.required],
-      height: ['', Validators.required],
+      weight: ['', Validators.required, Validators.min(40), Validators.max(200)],
+      height: ['', Validators.required, Validators.min(100), Validators.max(230)],
       birthDate: ['', Validators.required],
       country: ['', Validators.required]
+    }, { validators: this.passwordMatchValidator });
+
+    this.countryService.getAllCountries().subscribe({
+      next: (data) =>{
+        this.countries = data;
+      },
+      error: (err) => {
+        console.log("Something went wrong", err);
+      }
     });
   }
 
-  ngOnInit() {
-    this.countryService.getAllCountries().subscribe(
-      (data: Country[]) => {
-        this.countries = data;
-      },
-      (error) => {
-        console.error('Error al obtener los países', error);
-      }
-    );
+  passwordMatchValidator(control: AbstractControl): { [key: string]: boolean } | null {
+    const password = control.get('password');
+    const password2 = control.get('password2');
+    if (password && password2 && password.value !== password2.value) {
+      return { 'mismatch': true };
+    }
+    return null;
   }
 
-
   register() {
-    if (this.registerForm.valid) {
-      this.authService.register(this.registerForm.value).subscribe({
-        next: (response) => {
-          console.log("Registrado correctamente");
-          this.router.navigate(['/login']);
-        },
-        error: (err) => {
-          console.log("Algo ha fallado", err);
-        },
-        complete: () => {
-          console.log("Registro completado");
-        }
-      });
+    this.submitted = true;
+
+    if(this.registerForm.invalid){
+      return;
     }
+
+    const user: User = this.registerForm.value;
+
+    this.authService.register(user).subscribe({
+      next: (response) => {
+        console.log(user);
+      },
+      error: (err) => {
+        console.log("Something went wrong ", err)
+      },
+      complete: () => {
+        console.log("Register succesfully!")
+      }
+    });
+    
   }
 
 
